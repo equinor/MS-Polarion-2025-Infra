ba4cc6fdb447f2f792bf969192f76fb6fc7dad87
Tore Melberg <TmJr75@users.noreply.github.com>
2026-04-09 12:00:45 +0200

    Chore/runner (#50)
    * added otras
    
    * purge protection
    
    * concat ip rules
    
    * public
    
    * secrets test

--- custom-script/06-Install-AzureStorageExplorer.ps1
+++
@@ -1,8 +0,0 @@
-# Define the URL for Azure Storage Explorer
-$azureStorageExplorerUrl = "https://go.microsoft.com/fwlink/?LinkId=708343&clcid=0x409"
-
-# Download Azure Storage Explorer and overwrite if it already exists
-Invoke-WebRequest -Uri $azureStorageExplorerUrl -OutFile "C:\Appl\AzureStorageExplorer.exe" -UseBasicParsing
-
-# Install Azure Storage Explorer with silent and no restart options
-Start-Process -FilePath "C:\Appl\AzureStorageExplorer.exe" -ArgumentList "/quiet", "/norestart" -NoNewWindow -Wait
